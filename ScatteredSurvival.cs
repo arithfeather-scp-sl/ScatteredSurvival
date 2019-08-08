629160ad527b5537e33f7698b7a6ce50e834065e
Arifloof <dustswier@gmail.com>
2019-08-08 14:21:55 -0600

    Requires beta testing

--- ScatteredSurvival.cs
+++ ScatteredSurvival.cs
@@ -11,11 +11,8 @@ using ArithFeather.ArithSpawningKit.IndividualSpawns;
 using ArithFeather.ArithSpawningKit.RandomPlayerSpawning;
 using Smod2.EventHandlers;
 using ArithFeather.RandomItemSpawner;
+using UnityEngine.Networking.NetworkSystem;
 
-/// <summary>
-/// Remember to disable nuke
-/// tell them that you can't escape
-/// </summary>
 namespace ArithFeather.ScatteredSurvival
 {
 	[PluginDetails(
@@ -29,9 +26,10 @@ namespace ArithFeather.ScatteredSurvival
 		SmodMinor = 4,
 		SmodRevision = 0
 		)]
-	public class ScatteredSurvival : Plugin, IEventHandlerWaitingForPlayers, IEventHandlerSetConfig, IEventHandlerSetRole, IEventHandlerInitialAssignTeam,
-		IEventHandlerPlayerJoin, IEventHandlerContain106, IEventHandlerPlayerHurt, IEventHandlerCheckEscape,
-		IEventHandlerCallCommand
+	public class ScatteredSurvival : Plugin, IEventHandlerWaitingForPlayers, IEventHandlerSetConfig, IEventHandlerSetRole,
+		IEventHandlerInitialAssignTeam, IEventHandlerPlayerJoin, IEventHandlerContain106, IEventHandlerPlayerHurt,
+		IEventHandlerCheckEscape, IEventHandlerRoundStart, IEventHandlerCallCommand, IEventHandler079AddExp,
+		IEventHandlerGeneratorFinish, IEventHandlerWarheadChangeLever, IEventHandlerPlayerDie
 	{
 		public const string ModVersion = "1.04";
 
@@ -53,6 +51,7 @@ namespace ArithFeather.ScatteredSurvival
 		[ConfigOption] private readonly bool disablePlugin = false;
 		[ConfigOption] private readonly bool showGameStartMessage = true;
 		[ConfigOption] public readonly bool useDefaultConfig = true;
+		[ConfigOption] private readonly float expMultiplier = 10;
 
 		// Item Spawning addition
 		[ConfigOption] private readonly int[] onSpawnItems = new int[] { 0, 4, 9 };
@@ -62,7 +61,9 @@ namespace ArithFeather.ScatteredSurvival
 		//+ Round Data
 
 		private bool femurBroke;
-
+		private int generatorsActivated;
+		private bool roundStarted;
+		private bool endGame;
 		private Broadcast cachedBroadcast;
 
 		// Plugin Methods
@@ -92,11 +93,17 @@ namespace ArithFeather.ScatteredSurvival
 				//               ev.Value = true;
 				//break;
 
+				case "scp079_disable":
+					ev.Value = true;
+					break;
 				case "smart_class_picker":
 					ev.Value = false;
 					break;
+				//case "team_respawn_queue":
+				//	if (useDefaultConfig) ev.Value = "30333033333033333033333";
+				//	break;
 				case "team_respawn_queue":
-					if (useDefaultConfig) ev.Value = "30333033333033333033333";
+					if (useDefaultConfig) ev.Value = "300333033333033333033333";
 					break;
 
 				case "disable_dropping_empty_boxes":
@@ -126,7 +133,20 @@ namespace ArithFeather.ScatteredSurvival
 		{
 			if (ev.Command == "HELP")
 			{
-
+				ev.ReturnMessage = $"Scattered Survival Mode v{ScatteredSurvival.ModVersion}\n" +
+								   "https://discord.gg/DunUU82\n" +
+								   "Scientists will spawn randomly in Entrance and Light Containment Zone.\n" +
+								   "\n" +
+								   "SCP win if the player's lives reach 0. Lives are shared between everyone.\n" +
+								   "\n" +
+								   "Items will spawn randomly across the map.\n" +
+								   "You can not escape the facility. Decontamination and nuke are disabled.\n" +
+								   "Unless all 5 generators are active,dead SCP will respawn as SCP079 with 10x experience gain. SCP079 does not need to be killed to win.\n" +
+								   "SCP106 pelvis breaker no longer kills 106. Instead, it reduces their current HP by half.\n" +
+								   "--Rules--\n" +
+								   "1.Please don't group up with other teams.\n" +
+								   "2.Don't harass other players. Instant ban\n" +
+								   "3.Players found cheating will be recorded and reported for global ban.";
 			}
 		}
 
@@ -136,10 +156,11 @@ namespace ArithFeather.ScatteredSurvival
 			{
 				try
 				{
-					PersonalBroadcast(ev.Player, 10,
+					PersonalBroadcast(ev.Player, 8,
 						$"<size={ServerInfoSize}><color={ServerInfoColor}>Welcome to <color={ServerHighLightColor}>Scattered Survival v{ModVersion}!</color> Press ` to open the console and enter '<color={ServerHighLightColor}>.help</color>' for mod information!</color></size>");
-					PersonalBroadcast(ev.Player, 10,
+					PersonalBroadcast(ev.Player, 8,
 						$"<size={ServerInfoSize}><color={ServerInfoColor}>If you like the plugin, join the discord for updates!\n <color={ServerHighLightColor}>https://discord.gg/DunUU82</color></color></size>");
+					PersonalBroadcast(ev.Player, 8, "Nuke and escaping are disabled.");
 				}
 				catch
 				{
@@ -158,16 +179,55 @@ namespace ArithFeather.ScatteredSurvival
 
 			cachedBroadcast = GameObject.Find("Host").GetComponent<Broadcast>();
 			femurBroke = false;
+			roundStarted = false;
+			generatorsActivated = 0;
+			endGame = false;
 		}
 
 		public void OnAssignTeam(PlayerInitialAssignTeamEvent ev)
 		{
 			var player = ev.Player;
 			var spawner = ArithSpawningKit.RandomPlayerSpawning.RandomPlayerSpawning.Instance;
-			
+
 			if (ev.Team != Smod2.API.Team.SCP)
 			{
 				spawner.MakePlayerNextSpawnRandom(player, Role.SCIENTIST);
+			}
+		}
+
+		public void OnRoundStart(RoundStartEvent ev)
+		{
+			roundStarted = true;
+			Server.Round.Stats.ScientistsEscaped = 1;
+
+			RandomItemSpawner.RandomItemSpawner.Instance.ItemSpawning.SpawnItems(2, ZoneType.ENTRANCE, ItemType.MAJOR_SCIENTIST_KEYCARD, true);
+		}
+
+		public void OnPlayerDie(PlayerDeathEvent ev)
+		{
+			var player = ev.Player;
+			if (player.TeamRole.Team == Smod2.API.Team.SCP && player.TeamRole.Role != Role.SCP_079)
+			{
+				var scpPlayers = Server.GetPlayers(Smod2.API.Team.SCP);
+				var scpCount = scpPlayers.Count;
+				var scpAlive = 0;
+
+				for (int i = 0; i < scpCount; i++)
+				{
+					if (scpPlayers[i].TeamRole.Role != Role.SCP_079)
+						scpAlive++;
+				}
+
+				if (scpAlive <= 1)
+				{
+					generatorsActivated = 5;
+
+					for (int i = 0; i < scpCount; i++)
+					{
+						var scp = scpPlayers[i];
+						if (scp.TeamRole.Role == Role.SCP_079) scp.ChangeRole(Role.SPECTATOR);
+					}
+				}
 			}
 		}
 
@@ -186,9 +246,9 @@ namespace ArithFeather.ScatteredSurvival
 			// Send broadcast for roles.
 			else if (ev.TeamRole.Role == Role.SCIENTIST)
 			{
-				PersonalBroadcast(player, 10, $"<size={ClassInfoSize}><color={ClassInfoColor}>Kill the SCP before your lives reach 0. Lives are shared.</color></size>");
+				PersonalBroadcast(player, 10, $"<size={ClassInfoSize}><color={ClassInfoColor}>Kill the SCP before your lives reach 0. Lives are shared. Escape is disabled.</color></size>");
 
-				if (cachedSpawnInfo != null && cachedSpawnInfo.PlayerID == ev.Player.PlayerId)
+				if (roundStarted && cachedSpawnInfo != null && cachedSpawnInfo.PlayerID == ev.Player.PlayerId)
 				{
 					var itemSpawner = RandomItemSpawner.RandomItemSpawner.Instance.ItemSpawning;
 					itemSpawner.CheckSpawns();
@@ -205,6 +265,9 @@ namespace ArithFeather.ScatteredSurvival
 				}
 			}
 		}
+
+		public void On079AddExp(Player079AddExpEvent ev) => ev.ExpToAdd *= expMultiplier;
+		public void OnGeneratorFinish(GeneratorFinishEvent ev) => generatorsActivated++;
 
 		/// <summary>
 		/// Instead of killing him instantly, damage him for half his current HP
@@ -233,14 +296,24 @@ namespace ArithFeather.ScatteredSurvival
 			if (ev.Player.TeamRole.Role == Role.SCP_106 && ev.DamageType == DamageType.CONTAIN) ev.Damage = 0;
 		}
 
+		public void OnChangeLever(WarheadChangeLeverEvent ev) => ev.Allow = false;
+
 		// Custom Event
 		private void OnSpawnPlayer(DeadPlayer deadPlayer)
 		{
+
 			var player = deadPlayer.Player;
 
-			RandomPlayerSpawning.Instance.UpdateFreeSpawns(1);
-			RandomPlayerSpawning.Instance.MakePlayerNextSpawnRandom(player, Role.SCIENTIST);
-			player.ChangeRole(Role.SCIENTIST);
+			if (deadPlayer.PreviousTeam == Smod2.API.Team.SCP && generatorsActivated != 5)
+			{
+				player.ChangeRole(Role.SCP_079);
+			}
+			else
+			{
+				RandomPlayerSpawning.Instance.UpdateFreeSpawns(1);
+				cachedSpawnInfo = RandomPlayerSpawning.Instance.MakePlayerNextSpawnRandom(player, Role.SCIENTIST);
+				player.ChangeRole(Role.SCIENTIST);
+			}
 		}
 
 		#endregion
